5d91542227265c8b197e96b4102b95aca94c5d34
macbook <macbook@macbooks-MBP.tclwifi.net>
2024-11-13 21:43:37 +0300

    add service just for browser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject, NgZone, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NotificationService } from './service/notification-service'
+import {NotificationService } from './service/notification-service';
+import { isPlatformBrowser } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,10 +12,19 @@ import {NotificationService } from './service/notification-service'
 })
 export class AppComponent {
   title = 'browser-background-notification';
-  constructor(private notificationService: NotificationService) {}
+  constructor(private notificationService: NotificationService,
+    @Inject(PLATFORM_ID) private platformId: Object, private zone: NgZone
+  ) {}
+
 
   ngOnInit(): void {
-    this.notificationService.requestPermission();
-    this.notificationService.registerPeriodicSync();
+    if (isPlatformBrowser(this.platformId)) {
+      this.zone.run(() => {
+        // This will run only in the browser and ensures it's within the Angular zone
+        console.log(navigator.userAgent,'SGENEEEE');
+        this.notificationService.requestPermission();
+        this.notificationService.registerPeriodicSync();
+      });
+    }
   }
 }
